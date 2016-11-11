fe65ecc187ac3f8c9540784136c8ca4ca45e5b67
Jarrod Chesney <jarrod.chesney@synerty.com>
2016-11-11 10:55:47 +1000

    Updated all make_deploy scripts to use the bamboo date

--- bamboo_ut.sh
+++ bamboo_ut.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 PYTHONPATH="`pwd`/peek_platform/src"
 PYTHONPATH="$PYTHONPATH:`pwd`/rapui/src"
@@ -8,16 +8,13 @@ export PYTHONPATH
 UT_DIRS="peek_agent"
 
 export PATH=/home/bamboo/pyenvs/py_ut/bin:$PATH
-
 FILES=`grep -lR unittest.TestCase $UT_DIRS`
 echo "Running unit tests in files:"
 echo $FILES
-
 JUNIT_DIR=.junit
 mkdir ${JUNIT_DIR}
-
 OUT=${JUNIT_DIR}/trial.xml
-
-trial --reporter=subunit ${FILES} | subunit-1to2 | subunit2junitxml -o $OUT
-
+alias 2junitxml="subunit2junitxml -o $OUT"
+trial --reporter=subunit ${FILES} | subunit-1to2 | 2junitxml
 echo 0
+
