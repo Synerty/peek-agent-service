6ce7e51a77e12b6aec08ef5071af7ed5927366da
Jarrod Chesney <jarrod.chesney@synerty.com>
2016-11-11 10:25:04 +1000

    Remvoed use of alias from bamboo_ut.sh script

--- bamboo_ut.sh
+++ bamboo_ut.sh
@@ -8,12 +8,16 @@ export PYTHONPATH
 UT_DIRS="peek_agent"
 
 export PATH=/home/bamboo/pyenvs/py_ut/bin:$PATH
+
 FILES=`grep -lR unittest.TestCase $UT_DIRS`
 echo "Running unit tests in files:"
 echo $FILES
+
 JUNIT_DIR=.junit
 mkdir ${JUNIT_DIR}
+
 OUT=${JUNIT_DIR}/trial.xml
-alias 2junitxml="subunit2junitxml -o $OUT"
-trial --reporter=subunit ${FILES} | subunit-1to2 | 2junitxml
+
+trial --reporter=subunit ${FILES} | subunit-1to2 | subunit2junitxml -o $OUT
+
 echo 0
