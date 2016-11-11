e34f38c9345634dcda998b86bbf7af0e4fdd2aee
Jarrod Chesney <jarrod.chesney@synerty.com>
2016-11-11 10:12:28 +1000

    Added bamboo_ut.sh

---
+++ bamboo_ut.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+PYTHONPATH="`pwd`/peek_platform/src"
+PYTHONPATH="$PYTHONPATH:`pwd`/rapui/src"
+PYTHONPATH="$PYTHONPATH:`pwd`/peek_agent/src"
+export PYTHONPATH
+
+UT_DIRS="peek_agent"
+
+export PATH=/home/bamboo/pyenvs/py_ut/bin:$PATH
+FILES=`grep -lR unittest.TestCase $UT_DIRS`
+echo "Running unit tests in files:"
+echo $FILES
+JUNIT_DIR=.junit
+mkdir ${JUNIT_DIR}
+OUT=${JUNIT_DIR}/trial.xml
+alias 2junitxml="subunit2junitxml -o $OUT"
+trial --reporter=subunit ${FILES} | subunit-1to2 | 2junitxml
+echo 0
