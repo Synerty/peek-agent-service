ce18b21dd552983fc2ee256621f86197200a4134
Jarrod Chesney <jarrod.chesney@synerty.com>
2016-12-18 23:50:32 +1000

    Debugging package drag and drop update

--- peek_agent/sw_install/PluginSwInstallManager.py
+++ peek_agent/sw_install/PluginSwInstallManager.py
@@ -1,8 +1,8 @@
 from peek_agent.plugin.AgentPluginLoader import agentPluginLoader
-from peek_platform.sw_install.PluginSwInstallManagerBase import PluginSwInstallManagerBase
+from peek_platform.sw_install.PluginSwInstallManagerABC import PluginSwInstallManagerABC
 
 
-class PluginSwInstallManager(PluginSwInstallManagerBase):
+class PluginSwInstallManager(PluginSwInstallManagerABC):
     def notifyOfPluginVersionUpdate(self, pluginName, targetVersion):
         agentPluginLoader.loadPlugin(pluginName)
 
