51d598166d66b16546c55b199f305d1a05fde7d2
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-04-22 15:55:53 +1000

    Updated service name
    ORMOB-600

--- peek_agent/winsvc_peek_agent.py
+++ peek_agent/winsvc_peek_agent.py
@@ -8,8 +8,9 @@ logger = logging.getLogger(__name__)
 
 
 class PeekSvc(win32serviceutil.ServiceFramework):
-    _svc_name_ = "peek_agent"
+    _svc_name_ = "peek-agent"
     _svc_display_name_ = "Peek Agent "  # + peek_agent.__version__
+    _exe_args_ = IS_WI
 
     def __init__(self, args):
         win32serviceutil.ServiceFramework.__init__(self, args)
@@ -29,15 +30,6 @@ class PeekSvc(win32serviceutil.ServiceFramework):
     def SvcDoRun(self):
         self.ReportServiceStatus(win32service.SERVICE_START_PENDING)
         try:
-            # Patch the restart method for windows services
-            class _Restart:
-                def _restartProcess(self):
-                    reactor.callFromThread(reactor.stop)
-
-            # Patch the restart call for windows
-            from peek_platform.sw_install.PeekSwInstallManagerABC import \
-                PeekSwInstallManagerABC
-            PeekSwInstallManagerABC.restartProcess = _Restart._restartProcess
 
             reactor.callLater(1, self._notifyOfStart)
 
